d33a7427b5e7bfd28067e928e606e27c40a76ff5
phantekzy <mainilotfi@gmail.com>
2026-02-16 02:48:27 +0100

    fixed some bugs

--- src/ui.rs
+++ src/ui.rs
@@ -1,35 +1,27 @@
 use crate::system::SysStats;
-
 use ratatui::{
     Frame,
     layout::{Constraint, Direction, Layout},
-    style::{Color, Style},
-    widgets::{Block, Borders, Gauge, List, ListItem},
+    style::{Color, Modifier, Style},
+    widgets::{Block, Borders, Gauge, List, ListItem, Paragraph, Sparkline},
 };
+
 pub fn render(f: &mut Frame, stats: &SysStats) {
     let main_chunks = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
-            //CPU
             Constraint::Length(3),
-            //RAM
             Constraint::Length(3),
-            // PROCESSES AND NET
             Constraint::Min(10),
         ])
         .split(f.area());
 
     let bottom_chunks = Layout::default()
         .direction(Direction::Horizontal)
-        .constraints([
-            // Processes
-            Constraint::Percentage(50),
-            // Network
-            Constraint::Percentage(50),
-        ])
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
         .split(main_chunks[2]);
 
-    // CPU USAGE
+    // 1. CPU Gauge
     f.render_widget(
         Gauge::default()
             .block(Block::default().title(" CPU ").borders(Borders::ALL))
@@ -38,7 +30,7 @@ pub fn render(f: &mut Frame, stats: &SysStats) {
         main_chunks[0],
     );
 
-    // RAM USAGE
+    // 2. RAM Gauge
     let mem_p = (stats.mem_used as f64 / stats.mem_total as f64 * 100.0) as u16;
     f.render_widget(
         Gauge::default()
@@ -48,7 +40,7 @@ pub fn render(f: &mut Frame, stats: &SysStats) {
         main_chunks[1],
     );
 
-    // PROCESSES LIST
+    // 3. Process List
     let items: Vec<ListItem> = stats
         .processes
         .iter()
@@ -56,11 +48,26 @@ pub fn render(f: &mut Frame, stats: &SysStats) {
         .collect();
 
     let process_list = List::new(items)
-        .Block(
+        .block(
             Block::default()
                 .title(" Top Processes (Mem) ")
                 .borders(Borders::ALL),
         )
         .style(Style::default().fg(Color::White));
     f.render_widget(process_list, bottom_chunks[0]);
+
+    // 4. Network Info
+    let net_text = format!(
+        "\n IN:  {} KB/s\n OUT: {} KB/s\n\n Cache Stats: Optimized\n GPU: Detected",
+        stats.net_in / 1024,
+        stats.net_out / 1024
+    );
+    let net_para = Paragraph::new(net_text)
+        .block(
+            Block::default()
+                .title(" Network & Hardware ")
+                .borders(Borders::ALL),
+        )
+        .style(Style::default().fg(Color::Yellow));
+    f.render_widget(net_para, bottom_chunks[1]);
 }
