3519cc68c31fc03a585cd93f6fe3fb2c5fdcf633
phantekzy <mainilotfi@gmail.com>
2026-02-16 02:22:11 +0100

    lets finish it

--- src/main.rs
+++ src/main.rs
@@ -25,4 +25,20 @@ fn main() -> Result<(), io::Error> {
     let mut sys = System::new_all();
     let tick_rate = Duration::from_millis(500);
     let mut last_tick = Instant::now();
+
+    loop {
+        let stats = system::get_stats(&mut sys);
+        terminal.draw(|f| ui::render(f, &stats))?;
+
+        let timeout = tick_rate
+            .checked_sub(last_tick.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
+
+        if event::poll(timeout)? {
+            if let Event::Key(key) = event::read()? {
+                if let KeyCode::Char('q') = key.code {
+                    break;
+                }
+            }
+        }
 }
