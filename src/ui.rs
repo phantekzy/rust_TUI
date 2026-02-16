bad26a37162878301f3e372165c2a458839ca465
phantekzy <mainilotfi@gmail.com>
2026-02-16 02:38:57 +0100

    working on the Layout

--- src/ui.rs
+++ src/ui.rs
@@ -1,1 +1,15 @@
-pub fn render(f: &mut Frame, stats: &SysStats) {}
+use crate::system::SysStats;
+use ratatui::layout::{Constraint, Direction, Layout};
+pub fn render(f: &mut Frame, stats: &SysStats) {
+    let main_chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            //CPU
+            Constraint::Length(3),
+            //RAM
+            Constraint::Length(3),
+            // PROCESSES AND NET
+            Constraint::Min(10),
+        ])
+        .split(f.area());
+}
