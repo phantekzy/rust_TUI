187b83957d9204cd3e8a935e99dd68241a3438f2
phantekzy <mainilotfi@gmail.com>
2026-02-16 01:57:17 +0100

    Working on the rendering

--- src/ui.rs
+++ src/ui.rs
@@ -1,5 +1,21 @@
+use crate::system::SysStats;
 use ratatui::{
     Frame,
     layout::{Constraint, Direction, Layout},
     widgets::{Block, Borders, Gauge, Paragraph},
 };
+
+pub fn render(f: &mut Frame, stats: &SysStats) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(2)
+        .constraints([
+            // CPU
+            Constraint::Length(3),
+            // RAM
+            Constraint::Length(3),
+            // INFO
+            Constraint::Min(0),
+        ])
+        .split(f.size());
+}
