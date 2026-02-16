609162a58921eae2af61c3f5343ea1fcfeb2464c
phantekzy <mainilotfi@gmail.com>
2026-02-16 02:21:26 +0100

    lets finish it

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use std::io;
+use std::{
+    alloc::System,
+    io,
+    time::{Duration, Instant},
+};
 
 use crossterm::{
     execute,
     terminal::{self, EnterAlternateScreen, enable_raw_mode},
 };
 use ratatui::{Terminal, backend, prelude::CrosstermBackend};
+use sysinfo::System;
 
 mod system;
 mod ui;
@@ -16,4 +21,8 @@ fn main() -> Result<(), io::Error> {
     execute!(stdout, EnterAlternateScreen)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
+
+    let mut sys = System::new_all();
+    let tick_rate = Duration::from_millis(500);
+    let mut last_tick = Instant::now();
 }
